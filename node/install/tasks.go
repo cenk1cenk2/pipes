3d61c886ef5902c2d0bc6814a1b35a13839901cd
Cenk Kılıç <cenk@kilic.dev>
2022-08-04 12:17:23 +0200

    fix: update commands

--- node/install/tasks.go
+++ node/install/tasks.go
@@ -3,20 +3,20 @@ package install
 import (
 	"strings"
 
-	pipe "gitlab.kilic.dev/devops/pipes/node/setup"
+	"gitlab.kilic.dev/devops/pipes/node/setup"
 	. "gitlab.kilic.dev/libraries/plumber/v3"
 )
 
 func InstallNodeDependencies(tl *TaskList[Pipe]) *Task[Pipe] {
 	return tl.CreateTask("install").
 		Set(func(t *Task[Pipe]) error {
-			t.CreateCommand(pipe.TL.Pipe.Ctx.PackageManager.Exe).Set(func(c *Command[Pipe]) error {
+			t.CreateCommand(setup.TL.Pipe.Ctx.PackageManager.Exe).Set(func(c *Command[Pipe]) error {
 				if TL.Pipe.NodeInstall.UseLockFile {
-					c.AppendArgs(pipe.TL.Pipe.Ctx.PackageManager.Commands.InstallWithLock...)
+					c.AppendArgs(setup.TL.Pipe.Ctx.PackageManager.Commands.InstallWithLock...)
 
 					t.Log.Debugln("Using lockfile for installation.")
 				} else {
-					c.AppendArgs(pipe.TL.Pipe.Ctx.PackageManager.Commands.Install...)
+					c.AppendArgs(setup.TL.Pipe.Ctx.PackageManager.Commands.Install...)
 
 					t.Log.Debugln("Installing dependencies without a lockfile.")
 				}
