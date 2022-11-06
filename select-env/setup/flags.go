3aba50100338b0df4b6e192903923fc30ea6075b
Cenk Kılıç <cenk@kilic.dev>
2022-11-06 23:33:57 +0100

    fix: update deps

--- select-env/setup/flags.go
+++ select-env/setup/flags.go
@@ -57,7 +57,7 @@ var Flags = TL.Plumber.AppendFlags(flags.NewGitFlags(
 func ProcessFlags(tl *TaskList[Pipe]) error {
 	// setup selection of environment conditions
 	if v := tl.CliContext.String("environment.conditions"); v != "" {
-		if err := json.Unmarshal([]byte(v), &TL.Pipe.Conditions); err != nil {
+		if err := json.Unmarshal([]byte(v), &tl.Pipe.Conditions); err != nil {
 			return fmt.Errorf("Can not unmarshal environment conditions: %w", err)
 		}
 	}
