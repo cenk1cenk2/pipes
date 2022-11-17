de654ec380e1953743787c143c64f0bd30c39979
Cenk Kılıç <cenk@kilic.dev>
2022-11-17 18:36:39 +0100

    fix: update logical error on commands

--- node/run/pipe.go
+++ node/run/pipe.go
@@ -32,12 +32,14 @@ func New(p *Plumber) *TaskList[Pipe] {
 	return TL.New(p).
 		SetName("node", "run").
 		ShouldRunBefore(func(tl *TaskList[Pipe]) error {
-			args := tl.CliContext.Args().Slice()
-			if len(args) < 1 {
-				return fmt.Errorf("Arguments are needed to run a specific script.")
-			}
+			if TL.Pipe.NodeCommand.Script == "" {
+				args := tl.CliContext.Args().Slice()
+				if len(args) < 1 {
+					return fmt.Errorf("Arguments are needed to run a specific script.")
+				}
 
-			TL.Pipe.NodeCommand.Script, TL.Pipe.NodeCommand.ScriptArgs = args[0], strings.Join(args[1:], " ")
+				TL.Pipe.NodeCommand.Script, TL.Pipe.NodeCommand.ScriptArgs = args[0], strings.Join(args[1:], " ")
+			}
 
 			return nil
 		}).
