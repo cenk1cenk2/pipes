10ba28314194f820baece8ca2677ab776ea69ae4
Cenk Kılıç <cenk@kilic.dev>
2025-10-26 11:48:17 +0100

    fix: properly read charts

--- helm/setup/tasks.go
+++ helm/setup/tasks.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	. "github.com/cenk1cenk2/plumber/v6"
-	"gitlab.kilic.dev/devops/pipes/go/setup"
 	helmv2loader "helm.sh/helm/v4/pkg/chart/v2/loader"
 )
 
@@ -27,11 +26,11 @@ func HelmVersion(tl *TaskList) *Task {
 func HelmLoadChart(tl *TaskList) *Task {
 	return tl.CreateTask("read").
 		Set(func(t *Task) error {
-			chart, err := helmv2loader.Load(setup.P.Cwd)
+			chart, err := helmv2loader.Load(P.Cwd)
 			if err != nil {
-				return fmt.Errorf("Error loading helm chart: %v in %s", err, setup.P.Cwd)
+				return fmt.Errorf("Error loading helm chart: %v in %s", err, P.Cwd)
 			} else if chart == nil {
-				return fmt.Errorf("Can not load helm chart: %s", setup.P.Cwd)
+				return fmt.Errorf("Can not load helm chart: %s", P.Cwd)
 			}
 
 			t.Log.Infof("Chart Name: %s", chart.Metadata.Name)
