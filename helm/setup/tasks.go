d2b6b0c2d652adcefc233b3c978bcd4deda8cdc5
Labrat <labrat@kilic.dev>
2026-09-10 20:43:17 +0200

    refactor: capture command output with the Capture builders

--- helm/setup/tasks.go
+++ helm/setup/tasks.go
@@ -3,7 +3,6 @@ package setup
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	. "github.com/cenk1cenk2/plumber/v7"
 	helmv2loader "helm.sh/helm/v4/pkg/chart/v2/loader"
@@ -25,14 +24,12 @@ func version(tl *TaskList) *Task {
 		Set(func(_ context.Context, t *Task) error {
 			t.CreateCommand("helm", "version").
 				SetLogLevel(LogLevelDebug, LogLevelDebug, LogLevelDebug).
+				CaptureOutput(&C.Version).
 				ShouldRunAfter(func(_ context.Context, c *Command) error {
-					C.Version = strings.TrimSpace(strings.Join(c.GetCombinedStream(), "\n"))
-
 					c.Log.Info(fmt.Sprintf("helm version: %s", C.Version))
 
 					return nil
 				}).
-				EnableStreamRecording().
 				AddSelfToTheTask()
 
 			return nil
